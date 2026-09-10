de9f18ca14a7f5ed905fd750d83458af9915a1ef
easwarkumarvb <easwarkumarvb@gmail.com>
2026-09-10 14:07:39 +0530

    test: model Supabase postgres owner role

--- qa/browser_rpc_privilege_base.sql
+++ qa/browser_rpc_privilege_base.sql
@@ -1,4 +1,5 @@
-do $$ begin
+do $ begin
+  if not exists(select 1 from pg_roles where rolname='postgres') then create role postgres; end if;
   if not exists(select 1 from pg_roles where rolname='anon') then create role anon; end if;
   if not exists(select 1 from pg_roles where rolname='authenticated') then create role authenticated; end if;
   if not exists(select 1 from pg_roles where rolname='service_role') then create role service_role; end if;
