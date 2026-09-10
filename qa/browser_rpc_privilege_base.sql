3809ed1f18b0488c13ecf6f787dcd6105e852aff
easwarkumarvb <easwarkumarvb@gmail.com>
2026-09-10 14:11:07 +0530

    test: repair fixture dollar quoting

--- qa/browser_rpc_privilege_base.sql
+++ qa/browser_rpc_privilege_base.sql
@@ -1,4 +1,4 @@
-do $ begin
+do $$ begin
   if not exists(select 1 from pg_roles where rolname='postgres') then create role postgres; end if;
   if not exists(select 1 from pg_roles where rolname='anon') then create role anon; end if;
   if not exists(select 1 from pg_roles where rolname='authenticated') then create role authenticated; end if;
